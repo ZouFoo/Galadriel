cdd506c127b9f3726dbb37762c48078385f70a7e
BigFoo <1970226903@qq.com>
2020-01-03 17:47:14 +0800

    test

--- src/views/KengWang.vue
+++ src/views/KengWang.vue
@@ -539,15 +539,31 @@
         <div style="padding: 10px"><a
                 href="http://audioplay.html5.qq.com/?aid=1_rd003q4BfU3HYZaO&tid=1_rd0009vMju1cN79F&q=h" target="_blank">下安南（二）通灵神兽施援手
             还朝玉马赠血绒</a></div>
-
+        <aplayer autoplay :music = music />
     </div>
 </template>
+
 <script>
     // @ is an alias to /src
+    import Aplayer from 'vue-aplayer'
 
     export default {
         name: 'kengwang',
-        components: {}
+        components: {
+            Aplayer
+        },
+        data() {
+            return {
+                music: {
+                    title: 'yangchenchenbabybaby',
+                    author: 'AbelEthan',
+                    src: 'https://demeter-public.oss-cn-beijing.aliyuncs.com/music/%E8%8E%AB%E6%96%87%E8%94%9A-%E5%BF%BD%E7%84%B6%E4%B9%8B%E9%97%B4-%E5%8D%95%E6%9B%B2-%E6%94%B6%E5%BD%95%E5%9C%A8%E4%B8%93%E8%BE%91%E3%80%8A%E5%B0%B1%E6%98%AF%20%E8%8E%AB%E6%96%87%E8%94%9A%E3%80%8B%E4%B8%AD_%E5%8D%83%E5%8D%83%E9%9F%B3%E4%B9%90-%E5%90%AC%E8%A7%81%E4%B8%96%E7%95%8C.mp3',
+                    pic: 'https://scontent-itm1-1.cdninstagram.com/v/t51.2885-15/sh0.08/e35/p640x640/71312831_166578364420745_5588147026418538631_n.jpg?_nc_ht=scontent-itm1-1.cdninstagram.com&_nc_cat=105&_nc_ohc=vf-1hYEfhikAX8Ye6ni&oh=11114f9b736feac6bc129d8011999578&oe=5EB03E72',
+                    showLrc: '[00:00.85]Hush \n[00:01.21]魔幻力量&丁当[00:03.85]LRC:小二哥首发QQ：278856759[00:12.84]Let’s do it[00:14.26]你美得过火 我强烈怀疑你基因在玩火[00:20.61]纸包不住你的火[00:22.38]我是飞蛾你赴汤蹈火[00:26.74]趁火打劫的是禽兽[00:28.11]禽兽不如的才隔岸观火[00:29.84]星星之火能燎原 你太撩人[00:31.92]想像煽情得如荼如火[00:34.37]远水救不了近火[00:36.45]地球上只有你能够帮我灭火',
+                    lrc: '[00:00.00]lrc here\n[00:01.00]aplayer'
+                }
+            }
+        }
     }
 </script>
 
