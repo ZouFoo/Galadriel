9523da1fc1b6de1f4eb92231a95b0b1c20a4bead
ZouFoo <1970226903@qq.com>
2022-06-29 11:12:50 +0800

    更改一些东西

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,19 @@
 <template>
     <div id="app">
-        <Navigation></Navigation>
+        <div id="background"></div>
+        <!-- <Navigation></Navigation> -->
         <router-view/>
     </div>
 </template>
 
 <script>
     // @ is an alias to /src
-    import Navigation from './components/Navigation.vue'
+    // import Navigation from './components/Navigation.vue'
 
     export default {
         name: 'home',
         components: {
-            Navigation
+            // Navigation
         }
     }
 </script>
@@ -25,5 +26,14 @@
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
         color: #2c3e50;
+    }
+
+    #background {
+        background: url("./assets/background.jpg") no-repeat;
+        background-position: center;
+        height: 100%;
+        width: 100%;
+        background-size: cover;
+        position: fixed;
     }
 </style>
